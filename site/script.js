7f62b6663e3b9dc9c43ccc7e61ed6e8088009429
Fernand Pajot <fernand@pajot.net>
2023-12-09 15:11:59 -0700

    .

--- site/script.js
+++ site/script.js
@@ -4,7 +4,7 @@ const bookTitle = document.title
 const K = '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';
 
 let embs;
-fetch(`${bookTitle}_embs.json`)
+fetch(`${bookTitle.toLowerCase()}_embs.json`)
   .then(response => response.json())
   .then(json => embs = json);
 
